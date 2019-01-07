c557a8754c83846d22a0e29e53bd43e71461283b
Heloise Taillet <ht2394@columbia.edu>
2019-01-07 07:13:54 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
   # code here
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  
+
   arr.sort_by do |sentence|
     arr.split("").map do |letter|
       ESPERANTO_ALPHABET.index(letter)
