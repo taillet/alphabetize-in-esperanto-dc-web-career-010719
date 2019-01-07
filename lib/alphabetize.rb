725101045996d18ad86dab8a66713276f0f8205f
Heloise Taillet <ht2394@columbia.edu>
2019-01-07 07:15:52 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,8 +4,8 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
 
-  arr.sort_by do |sentence|
-    sentence.split("").map do |letter|
+  arr.sort_by do |phrase|
+    phrase.split("").map do |letter|
       ESPERANTO_ALPHABET.index(letter)
     end
   end
